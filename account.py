4324f69ca3cb2fd90e3cee78c360beeb779df732
matou <matou@taunusstein.net>
2011-02-14 03:26:24 +0100

    waving works. good night!

--- account.py
+++ account.py
@@ -9,6 +9,6 @@ def get_account_id(name):
     "returns the id of the specified account"
     for id in purple.PurpleAccountsGetAllActive():
         # i know this is ugly. but it's late and i want this to work.
-        if name == str(purple.PurpleAccountGetNameForDisplay(id)).split("/")[0]:
+        if name == str(purple.PurpleAccountGetUsername(id)).split("/")[0]:
             return id
     return -1
